ff525593552860c67fedfef09740c8692c63ccce
Gautham <balakrishnanrbe@gmail.com>
2021-10-31 15:55:48 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import About from "./component/About";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I am a React App!!</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h1>Hi I am a React App!!</h1>
+        <Home />
+        <About />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    console.log("Hii");
+
+    return (
+      <div>
+        <h1>Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
